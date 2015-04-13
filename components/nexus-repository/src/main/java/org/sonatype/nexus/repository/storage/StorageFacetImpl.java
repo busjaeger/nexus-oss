442b48b461198bef43c855999311c86681a9674d
Jason Dillon <jason@planet57.com>
2015-04-13 14:22:26 -0700

    null config on destroy for sanity

--- components/nexus-repository/src/main/java/org/sonatype/nexus/repository/storage/StorageFacetImpl.java
+++ components/nexus-repository/src/main/java/org/sonatype/nexus/repository/storage/StorageFacetImpl.java
@@ -202,6 +202,11 @@ public class StorageFacetImpl
   }
 
   @Override
+  protected void doDestroy() throws Exception {
+    config = null;
+  }
+
+  @Override
   protected void doDelete() throws Exception {
     // TODO: Make this a soft delete and cleanup later so it doesn't block for large repos.
     try (StorageTx tx = openStorageTx(false)) {
